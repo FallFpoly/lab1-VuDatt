38a7a26820a64960a4d640da14626e081add9aba
vudat <vudat@vudat>
2025-09-28 19:09:41 +0700

    Nop lab 1

--- Lab1/src/main/java/com/mycompany/lab1/bai2.java
+++ Lab1/src/main/java/com/mycompany/lab1/bai2.java
@@ -1,16 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.lab1;
 
-/**
- *
- * @author ICT
- */
+import java.util.Scanner;
+
+
 public class bai2 {
 
-    public static void main(String[] args) {
-          // sinh viên code ở đây
+   public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.print("Nhap chieu dai: ");
+        double chieuDai = sc.nextDouble();
+
+        System.out.print("Nhap chieu rong: ");
+        double chieuRong = sc.nextDouble();
+
+        
+        double chuVi = (chieuDai + chieuRong) * 2;
+
+        
+        double dienTich = chieuDai * chieuRong;
+
+        
+        double canhNho = Math.min(chieuDai, chieuRong);
+
+        
+        System.out.println("Chu vi hinh chu nhat la: " + chuVi);
+        System.out.println("Dien tich hinh chu nhat la: " + dienTich);
+        System.out.println("Canh nho hon la : " + canhNho);
+
+        sc.close();
     }
 }
